a76d8ecfee7e69d134da4b05d2102abb31c1e937
formatgit <zhukuilong1>
2018-09-13 16:23:30 +0800

    0

---
+++ src/main/java/com/controller/FirstController.java
@@ -0,0 +1,27 @@
+package com.controller;
+
+/*
+*@ClassName:FirstController
+ @Description:TODO
+ @Author:
+ @Date:2018/9/10 16:13 
+ @Version:v1.0
+*/
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/*表明这是一个controller*/
+@Controller
+@RequestMapping("/")
+public class FirstController {
+    //ResponseBody 以字符串响应
+    @ResponseBody
+    @RequestMapping("/hello.do")
+    public String hello(){
+            return "hello world";
+    }
+
+
+}
